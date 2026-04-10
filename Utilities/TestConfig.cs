2cdc7b0f54a32db95b4f2661cc4844602c9f532d
Logan Garbacki <lgarbacki@printscan.com>
2026-04-10 14:49:02 -0400

    Allure reporting (5/n) - Attempt to deploy allure reporting live automatically

--- Utilities/TestConfig.cs
+++ Utilities/TestConfig.cs
@@ -5,7 +5,7 @@ namespace SeleniumTestFramework
     public static class TestConfig
     {
         public const string BaseUrl = "https://www.logangarbacki.dev";
-        public const int ElementTimeoutSeconds = 10;
+        public const int ElementTimeoutSeconds = 6;
         public const int UrlTimeoutSeconds = 5;
     }
 }
